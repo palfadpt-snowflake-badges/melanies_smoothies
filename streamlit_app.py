3232e05f8aa97f883367a2b8d4f435367bb8482d
palfadpt-snowflake-badges <palf.adpt@gmail.com>
2026-05-26 14:17:06 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ if ingredients_list:
       #ingredients_string += fruit_chosen + ' '
 
   #API call
-      import requests  
+import requests  
       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
       st.text(smoothiefroot_response).json()
      
