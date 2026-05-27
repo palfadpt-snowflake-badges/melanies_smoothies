223e32f1e24d588d087ac60da348da087a049353
palfadpt-snowflake-badges <palf.adpt@gmail.com>
2026-05-27 10:14:22 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,8 +67,9 @@ if ingredients_list:
     # if ingredients_string:
     #     session.sql(my_insert_stmt).collect()
     #     st.success('Your Smoothie is ordered!', icon="✅");
-    # time_to_insert = st.button('Submit Order')
-
+   
+   time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅");
+      
